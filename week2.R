2c0cfe685ff82af22e23e7292147f1d402465e21
HANEEF MEERAN <haneefmrn@gmail.com>
2019-06-09 22:16:55 -0400

    Weekly practice

---
+++ week2.R
@@ -0,0 +1,45 @@
+
+setwd("~/Desktop/EXPL DATA ANALYSIS")
+
+library(readr)
+
+ozone <- read.csv("US EPA data 2017.csv")
+
+summary(ozone)
+
+str(ozone)
+
+head(ozone)
+
+tail(ozone)
+
+ozone
+
+nrow(ozone)
+
+ncol(ozone)
+
+mean(ozone$Observation.Count)
+
+
+head(ozone, 10)
+
+library(tidyverse)
+
+typeof ("Datum")
+
+head(ozone[, c(6:7, 10)])
+
+tail(ozone[, c(6:7, 10)])
+
+library(dplyr)
+
+table(ozone$Time.Local)
+
+select(ozone, State.Name) %>% unique %>% nrow
+
+unique(ozone$State.Name)
+
+summary(ozone$Sample.Measurement)
+
+
